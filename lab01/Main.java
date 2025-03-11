a7455760f270c4790eed0240577868b750dbebf1
Gabriel Mattias Antunes <ra281199@pernalonga.lab.ic.unicamp.br>
2025-03-11 15:43:59 -0300

    corrige erros, termina o programa

--- lab01/Main.java
+++ lab01/Main.java
@@ -2,9 +2,26 @@ public class Main{
     public static void main(String[] args){
         int l = 10, h = 10;
         Ambiente a;
+        Robo r1, r2;
 
         a = new Ambiente(l, h);
+        r1 = new Robo("Isaias", 0, 0);
+        r2 = new Robo("Gabriel", 3, 7);
 
-        System.out.println("oi");
+        r1.mover(3, 11);
+        r2.mover(-3, 1);
+
+        if (a.dentroDosLimites(r1.x_position, r1.y_position))
+            System.out.println(r1.robot_name + " está dentro dos limites.");
+        else
+            System.out.println(r1.robot_name + " não está dentro dos limites.");
+
+        if (a.dentroDosLimites(r2.x_position, r2.y_position))
+            System.out.println(r2.robot_name + " está dentro dos limites.");
+        else
+            System.out.println(r2.robot_name + " não está dentro dos limites.");
+
+        r1.exibirPosicao();
+        r2.exibirPosicao();
     }
 }
--- lab01/Robo.java
+++ lab01/Robo.java
@@ -1,23 +1,23 @@
 public class Robo {
-    private String robot_name;
-    private int x_position;
-    private int y_position;
+    public String robot_name;
+    public int x_position;
+    public int y_position;
     
-}
-
-//construtor
-public Robo(String n, int x, int y){
-    robot_name = n;
-    x_position = x;
-    y_position = y;
-}
-//metodo de acesso
-public mover(int delta_x, int delta_y){
-    x_position += delta_x;
-    y_position += delta_y;
-}
+    //construtor
+    public Robo(String n, int x, int y){
+        robot_name = n;
+        x_position = x;
+        y_position = y;
+    }
+    //metodo de acesso
+    public void mover(int delta_x, int delta_y){
+        this.x_position += delta_x;
+        this.y_position += delta_y;
+    }
 
 
-public exibirPosicao(){
-    System.out.println(x_position,y_position);
+    public void exibirPosicao(){
+        System.out.println(this.x_position + ", " + this.y_position);
+    }
 }
+
