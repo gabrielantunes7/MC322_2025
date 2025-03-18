f7c8dfd00a74e7c162b6400c4aea69037b070af7
Gabriel Mattias Antunes <ra281199@smurfs.lab.ic.unicamp.br>
2025-03-18 14:39:53 -0300

    Adiciona arquivos do lab02 e atualiza a classe Ambiente

---
+++ lab02/Ambiente.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+// Classe Ambiente
+// Usada para representar o ambiente em que objetos da classe robô estão inseridos
+public class Ambiente{
+    private int largura;
+    private int altura; 
+    private int altitude;
+    private ArrayList<Robo> robos; // ArrayList utilizado para guardar os robôs que estão no ambiente
+
+    // Inicializador
+    // Cria um ambiente com dada largura, altura e altitude e cria um ArrayList para os robôs
+    public Ambiente(int umaLargura, int umaAltura, int umaAltitude){
+        largura = umaLargura;
+        altura = umaAltura;
+        altitude = umaAltitude;
+        this.robos = new ArrayList<>();
+    }
+
+    // Adiciona um robô ao ambiente (coloca-o no ArrayList)
+    public void adicionarRobo(Robo r){
+        robos.add(r);
+    }
+
+    // Retorna true se dada posição está dentro dos limites do ambiente e false se não
+    public boolean dentroDosLimites(int x, int y, int z){
+        if ((x & y & z) >= 0){
+            if (x <= this.largura && y <= this.altura && z <= this.altura)
+                return true;
+        }
+        return false;
+    }
+}
